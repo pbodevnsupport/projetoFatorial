f998167dbf6e9dc7e13e672beb57f9677b04a581
Patrick Barboza Oliveira <patrickboza2017@gmail.com>
2022-01-28 18:25:33 -0300

    Alteracao para o calculo correto do fatorial de zero

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -1,7 +1,7 @@
 class TesteFatorial {
     public static void main (String[] args){
         Fatorial fat = new Fatorial();
-        int resultado = fat.CalcularFatorial(-5);
+        int resultado = fat.CalcularFatorial(0);
         System.out.println(resultado);
     }
 }
