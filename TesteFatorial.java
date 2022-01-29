2c7d7337172fbd018f0c3c182e301363edd53e13
Patrick Barboza Oliveira <patrickboza2017@gmail.com>
2022-01-28 21:41:05 -0300

    calculo do fatorial de 10 e renomeacao da variavel local

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ class TesteFatorial {
     // Comentario inserido em TesteFatorial.java
     public static void main (String[] args){
         Fatorial fat = new Fatorial();
-        int resultado = fat.CalcularFatorial(0);
-        System.out.println(resultado);
+        int r = fat.CalcularFatorial(10);
+        System.out.println(r);
     }
 }
