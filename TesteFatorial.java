3feac6a06ae6e43c9b9a6ada93c7b0b0c7c66c3a
Patrick Barboza Oliveira <patrickboza2017@gmail.com>
2022-01-28 21:39:39 -0300

    Calculo do fatorial de 4 em vez de 0

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,7 +2,7 @@ class TesteFatorial {
     // Comentario inserido em TesteFatorial.java
     public static void main (String[] args){
         Fatorial fat = new Fatorial();
-        int resultado = fat.CalcularFatorial(0);
+        int resultado = fat.CalcularFatorial(4);
         System.out.println(resultado);
     }
 }
